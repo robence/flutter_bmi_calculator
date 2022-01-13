198d35089138ba5f7ebceb101ad05a8343e915b9
Bence Rozsa <robence@gmail.com>
2022-01-13 15:44:53 +0100

    create result page ui

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bmi_calculator/constants/custom_colors.dart';
+import 'package:flutter_bmi_calculator/constants/font_styles.dart';
+import 'package:flutter_bmi_calculator/ui/reusable_card.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -8,6 +11,59 @@ class ResultPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI Calculator'.toUpperCase()),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 24.0),
+            child: Text(
+              "Your Result",
+              style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ReusableCard(
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    "Text",
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Value",
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Desc",
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () => Navigator.pushReplacementNamed(context, '/result'),
+            child: Container(
+              padding: const EdgeInsets.only(bottom: 12.0),
+              child: Center(
+                child: Text(
+                  'Re-Calculate'.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: FontStyles.button,
+                ),
+              ),
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              color: CustomColors.button,
+              height: 80.0,
+            ),
+          )
+        ],
       ),
     );
   }
